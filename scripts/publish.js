13be02e2687c93c40770e0c6cf51b051e23c5025
tiagojacinto <tiagojacintodev@gmail.com>
2025-01-27 17:28:05 +0000

    add publish modes

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,74 +1,127 @@
 const fs = require('fs');
 const { execSync } = require('child_process');
-const { input, checkbox } = require('@inquirer/prompts');
+const { input, checkbox, select } = require('@inquirer/prompts');
 
 (async () => {
-  const packages = await checkbox({
-    message: 'Select the packages to publish:',
-    choices: ['typescript', 'eslint'],
-  });
+  try {
+    const mode = await select({
+      message: 'Select the mode:',
+      choices: ['one', 'multiple'],
+    });
 
-  const commitMessage = await input({ message: 'Enter the commit message:' });
+    const packages = fs.readdirSync('./packages');
 
-  const packagesConfiguration = {};
+    if (mode === 'one') {
+      const package = await select({
+        message: 'Select the package to publish:',
+        choices: packages,
+      });
 
-  for (const package of packages) {
-    const cwd = `./packages/${package}`;
-    const packageJsonPath = `${cwd}/package.json`;
-    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
+      const commitMessage = await askForCommitMessage();
 
-    console.log(`Configure package: ${package}`);
-    console.log('Current version: ', packageJson.version);
-    const version = await input({ message: 'Enter the new version:' });
+      const packageJsonPath = getPackageJsonPath(package);
+      const packageJson = getPackageJson(packageJsonPath);
 
-    packagesConfiguration[package] = {
-      cwd,
-      currentVersion: packageJson.version,
-      newVersion: version,
-      packageJsonPath,
-      currentPackageJson: packageJson,
-      newPackageJson: {
-        ...packageJson,
-        version,
-      },
-    };
-  }
+      console.log('Current version: ', packageJson.version);
+      const version = await input({ message: 'Enter the new version:' });
 
-  for (const package of packages) {
-    const { cwd, packageJsonPath, newPackageJson } = packagesConfiguration[package];
+      const cwd = getPackageCwd(package);
 
-    try {
-      fs.writeFileSync(packageJsonPath, JSON.stringify(newPackageJson, null, 2));
-      console.log(`Version updated to ${version}`);
+      updatePackageJson(packageJsonPath, { ...packageJson, version });
 
       execSync('npm publish', { stdio: 'inherit', cwd });
 
       execSync('git add .', { stdio: 'inherit', cwd });
-    } catch (error) {
-      console.error('An error occurred:', error.message);
+
+      execSync(
+        `git commit -m "publish package ${package} with version ${version} | ${commitMessage}"`,
+        {
+          stdio: 'inherit',
+          cwd,
+        }
+      );
     }
 
-    console.log('----------------------------------------------------------');
-  }
+    if (mode === 'multiple') {
+      const selectedPackages = await checkbox({
+        message: 'Select the packages to publish:',
+        choices: packages,
+      });
 
-  execSync(
-    `git commit -m "publish packages [${packages.join(', ')}] | ${commitMessage}" ${Object.entries(
-      packagesConfiguration
-    )
-      .map(
-        ([name, { currentVersion, newVersion }]) =>
-          `-m "${name}: ${currentVersion} => ${newVersion}"`
-      )
-      .join(' ')}`,
-    {
+      const commitMessage = await askForCommitMessage();
+
+      const packagesConfiguration = {};
+
+      for (const package of selectedPackages) {
+        const packageJsonPath = getPackageJsonPath(package);
+        const packageJson = getPackageJson(packageJsonPath);
+
+        console.log(`Configure package: ${package}`);
+        console.log('Current version: ', packageJson.version);
+        const version = await input({ message: 'Enter the new version:' });
+
+        packagesConfiguration[package] = {
+          cwd: getPackageCwd(package),
+          currentVersion: packageJson.version,
+          newVersion: version,
+          packageJsonPath,
+          currentPackageJson: packageJson,
+          newPackageJson: {
+            ...packageJson,
+            version,
+          },
+        };
+      }
+
+      for (const package of selectedPackages) {
+        const { cwd, packageJsonPath, newPackageJson } = packagesConfiguration[package];
+
+        updatePackageJson(packageJsonPath, newPackageJson);
+
+        execSync('npm publish', { stdio: 'inherit', cwd });
+
+        execSync('git add .', { stdio: 'inherit', cwd });
+
+        console.log('----------------------------------------------------------');
+      }
+
+      execSync(
+        `git commit -m "publish packages [${selectedPackages.join(
+          ', '
+        )}] | ${commitMessage}" ${Object.entries(packagesConfiguration)
+          .map(
+            ([name, { currentVersion, newVersion }]) =>
+              `-m "${name}: ${currentVersion} => ${newVersion}"`
+          )
+          .join(' ')}`,
+        {
+          stdio: 'inherit',
+          cwd,
+        }
+      );
+    }
+
+    execSync('git push -u origin main', {
       stdio: 'inherit',
-      cwd,
-    }
-  );
-  execSync('git push -u origin main', {
-    stdio: 'inherit',
-    cwd,
+    });
+
+    console.log('Commit pushed to origin main');
+  } catch (error) {
+    console.error('An error occurred:', error.message);
+  }
+})();
+
+const askForCommitMessage = () =>
+  input({
+    message: 'Enter the commit message:',
   });
 
-  console.log('Commit pushed to origin main');
-})();
+const getPackageCwd = (package) => `./packages/${package}`;
+const getPackageJsonPath = (package) => `./packages/${package}/package.json`;
+const getPackageJson = (path) => JSON.parse(fs.readFileSync(path, 'utf-8'));
+
+const updatePackageJson = (path, newPackageJson) => {
+  fs.writeFileSync(path, JSON.stringify(newPackageJson, null, 2));
+
+  console.log(`Version updated to ${newPackageJson.version}`);
+};

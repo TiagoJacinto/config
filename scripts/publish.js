b1abfab1f866dccc2e8df6a877953a8f932c1a6a
tiagojacinto <tiagojacintodev@gmail.com>
2025-01-27 16:50:08 +0000

    commit once for all the packages published

--- scripts/publish.js
+++ scripts/publish.js
@@ -4,11 +4,11 @@ const { input, checkbox } = require('@inquirer/prompts');
 
 (async () => {
   const packages = await checkbox({
-    message: 'Select the packages to publish: ',
+    message: 'Select the packages to publish:',
     choices: ['typescript', 'eslint'],
   });
 
-  const commitMessage = await input({ message: 'Enter the commit message: ' });
+  const commitMessage = await input({ message: 'Enter the commit message:' });
 
   const packagesConfiguration = {};
 
@@ -19,43 +19,56 @@ const { input, checkbox } = require('@inquirer/prompts');
 
     console.log(`Configure package: ${package}`);
     console.log('Current version: ', packageJson.version);
-    const version = await input({ message: 'Enter the new version: ' });
-
-    packageJson.version = version;
+    const version = await input({ message: 'Enter the new version:' });
 
     packagesConfiguration[package] = {
       cwd,
+      currentVersion: packageJson.version,
+      newVersion: version,
       packageJsonPath,
-      packageJson,
+      currentPackageJson: packageJson,
+      newPackageJson: {
+        ...packageJson,
+        version,
+      },
     };
   }
 
   for (const package of packages) {
-    const { cwd, packageJsonPath, packageJson } = packagesConfiguration[package];
+    const { cwd, packageJsonPath, newPackageJson } = packagesConfiguration[package];
 
     try {
-      fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+      fs.writeFileSync(packageJsonPath, JSON.stringify(newPackageJson, null, 2));
       console.log(`Version updated to ${version}`);
 
       execSync('npm publish', { stdio: 'inherit', cwd });
 
       execSync('git add .', { stdio: 'inherit', cwd });
-      execSync(
-        `git commit -m "publish package ${package} with version ${version} | ${commitMessage}"`,
-        {
-          stdio: 'inherit',
-          cwd,
-        }
-      );
-      execSync('git push -u origin main', {
-        stdio: 'inherit',
-        cwd,
-      });
-      console.log('Commit pushed to origin main');
     } catch (error) {
       console.error('An error occurred:', error.message);
     }
 
     console.log('----------------------------------------------------------');
   }
+
+  execSync(
+    `git commit -m "publish packages [${packages.join(', ')}] | ${commitMessage}" ${Object.entries(
+      packagesConfiguration
+    )
+      .map(
+        ([name, { currentVersion, newVersion }]) =>
+          `-m "${name}: ${currentVersion} => ${newVersion}"`
+      )
+      .join(' ')}`,
+    {
+      stdio: 'inherit',
+      cwd,
+    }
+  );
+  execSync('git push -u origin main', {
+    stdio: 'inherit',
+    cwd,
+  });
+
+  console.log('Commit pushed to origin main');
 })();

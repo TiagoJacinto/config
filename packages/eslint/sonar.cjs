eb3cf55b994ccd607f51b64b8a4b99085c871531
Tiago <tiagojacintodev@gmail.com>
2025-02-24 13:29:58 +0000

    publish package eslint with version 0.1.9 | fix the no-duplicate-string option to a minimum threshold of 1 and divide the base value by the ratio

--- packages/eslint/sonar.cjs
+++ packages/eslint/sonar.cjs
@@ -5,7 +5,12 @@ module.exports = ({ ratios } = { ratios: { refactoring: 1 } }) => [
   require('eslint-plugin-sonarjs').configs.recommended,
   {
     rules: {
-      'sonarjs/no-duplicate-string': ['warn', { threshold: Math.round(3 * ratios.refactoring) }],
+      'sonarjs/no-duplicate-string': [
+        'warn',
+        {
+          threshold: ratios.refactoring <= 0 ? 1 : Math.max(1, Math.round(3 / ratios.refactoring)),
+        },
+      ],
     },
   },
 ];

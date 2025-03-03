cd8a21b8aa88e02f63816f33c6d263bc2b404e66
Tiago <tiagojacintodev@gmail.com>
2025-03-03 12:12:18 +0000

    publish package prettier with version 0.0.8 | implement config method for merging multiple configs

--- packages/prettier/src/index.ts
+++ packages/prettier/src/index.ts
@@ -1,9 +1,18 @@
-import base from './base.js';
-import svelte from './svelte.js';
-import tailwind from './tailwind.js';
+import { Config } from 'prettier';
+import base from './configs/base.js';
+import svelte from './configs/svelte.js';
+import tailwind from './configs/tailwind.js';
+import { reduce, mergeDeepWith, concat } from 'ramda';
 
 export default {
-  base,
-  svelte,
-  tailwind,
+  config: (...configs: Config[]) =>
+    reduce<Config, Config>(
+      mergeDeepWith((l, r) => (Array.isArray(l) && Array.isArray(r) ? concat(l, r) : l)),
+      {}
+    )(configs),
+  configs: {
+    base,
+    svelte,
+    tailwind,
+  },
 };

07dbec02edd08577595004a15af0c5b07ceb8bab
Tiago <tiagojacintodev@gmail.com>
2025-03-03 12:39:47 +0000

    publish packages [eslint, prettier] | add properties export support
    eslint: 0.3.7 => 0.3.8
    
    prettier: 0.0.8 => 0.0.9

--- packages/prettier/src/index.ts
+++ packages/prettier/src/index.ts
@@ -4,15 +4,20 @@ import svelte from './configs/svelte.js';
 import tailwind from './configs/tailwind.js';
 import { reduce, mergeDeepWith, concat } from 'ramda';
 
+const config = (...configs: Config[]) =>
+  reduce<Config, Config>(
+    mergeDeepWith((l, r) => (Array.isArray(l) && Array.isArray(r) ? concat(l, r) : l)),
+    {}
+  )(configs);
+
+const configs = {
+  base,
+  svelte,
+  tailwind,
+};
+
+export { config, configs };
 export default {
-  config: (...configs: Config[]) =>
-    reduce<Config, Config>(
-      mergeDeepWith((l, r) => (Array.isArray(l) && Array.isArray(r) ? concat(l, r) : l)),
-      {}
-    )(configs),
-  configs: {
-    base,
-    svelte,
-    tailwind,
-  },
+  config,
+  configs,
 };

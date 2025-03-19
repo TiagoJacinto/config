e37b6462dffb8f4b027bce334a4fb7cabe70c83c
Tiago <tiagojacintodev@gmail.com>
2025-03-19 11:12:37 +0000

    publish package prettier with version 0.1.1 | organize

--- packages/prettier/src/index.ts
+++ packages/prettier/src/index.ts
@@ -1,20 +1,14 @@
 import { Config } from 'prettier';
-import base from './configs/base.js';
-import svelte from './configs/svelte.js';
-import tailwind from './configs/tailwind.js';
+import configs from './configs/index.js';
 import { reduce, mergeDeepWith, concat } from 'ramda';
 
 const config = (...configs: Config[]) =>
   reduce<Config, Config>(
-    mergeDeepWith((l, r) => (Array.isArray(l) && Array.isArray(r) ? concat(l, r) : l)),
+    mergeDeepWith((left, right) =>
+      Array.isArray(left) && Array.isArray(right) ? concat(left, right) : left
+    ),
     {}
   )(configs);
-
-const configs = {
-  base,
-  svelte,
-  tailwind,
-};
 
 export { config, configs };
 export default {

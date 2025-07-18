6ea99ec207dabb869516f3205c26a1a1b38d9ab2
Tiago Jacinto <tiagojacintodev@gmail.com>
2025-07-18 16:00:37 +0100

    publish package jest with version 0.0.1 | define browser and node configs

---
+++ packages/jest/src/configs/browser.ts
@@ -0,0 +1,5 @@
+import type { Config } from 'jest';
+
+export default {
+  testEnvironment: 'jsdom',
+} satisfies Config;
---
+++ packages/jest/src/index.ts
@@ -0,0 +1,6 @@
+import configs from './configs/index.js';
+
+export { configs };
+export default {
+  configs,
+};
